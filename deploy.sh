c6e988cbaa8bd45f1e856f21bd827154bb480393
An <1225835221@qq.com>
2020-09-29 11:00:04 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git init
 git add -A
 git commit -m 'deploy'
 git push -f "https://${access_token}@gitee.com:anitt/vue-admin-ay.git" master:ay-pages
-start "https://gitee.com/chu1204505056/vue-admin-beautiful/pages"
+start "https://gitee.com/anitt/vue-admin-ay"
 git push -f "https://${access_token}@github.com:Anyi0958/vue-admin-ay.git" master:ay-pages
 cd -
 exec /bin/bash
